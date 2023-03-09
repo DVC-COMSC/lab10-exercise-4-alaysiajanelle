3d2998fdfa4155e373e1d133431fb7ad8f4f9037
aramirez4 <126624936+alaysiajanelle@users.noreply.github.com>
2023-03-09 02:47:42 +0000

    Celsius degree number

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+   ##################################################
+   # Comlete your code here
+   ##################################################
+    celcius = int(input('Enter the Celcius Degree: '))
+    farenheit = (6.0 / 3.0) * celcius + 35
+    print(f'The Farenheit is {farenheit:2f}')
+   
+pass
 
 
 if __name__ == '__main__':
